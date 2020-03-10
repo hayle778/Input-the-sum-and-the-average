c0e4cba602c14eee03566e8ca51d487e2fd19da0
Haylemelekot <HayNaS@Vickis-iMac.local>
2020-03-09 23:30:40 -0400

    ll

--- src/Gemm.java
+++ src/Gemm.java
@@ -6,26 +6,40 @@ Write a program in Java to input
 */
 public class Gemm {
 public static void main(String[]args) {
+    double avg = 0.0 ;
 
-      Scanner input = new Scanner(System.in);
-        int y;
-      System.out.println("Input the number: ");
-       y = input.nextInt();
+    Scanner input = new Scanner(System.in);
+    int y;
+    System.out.println("Input the number: ");
+    y = input.nextInt();
+    int sum = 0;
 
-       int sum = 0;
-       int k ;
-       for( k = 1; k <= y; k++){
-           sum += k;
-         // int total = sum;
-           System.out.println("" + k);
 
-         // finding the sum of all the value of k;
-      //y += 1;
+    int k = 0;
+    for (k = 1; k <= y; k++) {
+        sum += k;
+        // int total = sum;
+        //multiplication *= k;
 
-       }
-   //    System.out.println( "The Sum of y is: " + k );
+        System.out.println("The first n natural number: " + k);
 
-    System.out.println( "The Sum of y is: " + sum  );
+        // finding the sum of all the value of k;
+        //y += 1;
+        //  System.out.println("The multiplication of y is: " + multiplication);
+        //System.out.println( "The Sum of y is: " + sum  );
+    }
 
+    System.out.println("The Sum of y is: " + sum);
+    System.out.println("avg = "+ sum/y);
 }
+//public static double avg(int sum, int y){
+//     sum = 0;
+//    y = 0;
+//   double avg =  sum/y;
+//   System.out.println("avg = "+ avg);
+//    return avg;
+//}
+
+
+
 }
